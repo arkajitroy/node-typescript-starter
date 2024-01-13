0293d18b5368715d3e8906ca139bb826f193a0d3
arkajitroy <arkajitroy11.official@gmail.com>
2024-01-14 02:44:29 +0530

    FEAT: add user service and routes

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import userRoutes from "./users/users.routes";
+import authRoutes from "./auth/auth.routes";
 const router = Router();
 
 const AppRouter = (): Router => {
   userRoutes(router, "users");
+  authRoutes(router, "auth");
 
   // return
   return router;
--- src/routes/users/users.routes.ts
+++ src/routes/users/users.routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { apiController } from "../../api/controllers";
+import { registerUser } from "../../api/controllers/users/registerUser.controller";
 
 const userRoutes = (router: express.Router, subRoute: string) => {
   router.get(`/${subRoute}/get-user`, apiController.user.getUser);
