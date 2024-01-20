715d3b8f47608a42e5d8e2ee7461711b3e0cf19d
arkajitroy <arkajitroy11.official@gmail.com>
2024-01-20 21:01:10 +0530

    FEAT: addeed loginUser api

--- src/api/controllers/users/getUser.controller.ts
+++ src/api/controllers/users/getUser.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import UserModel from "../../../model/users/Users.model";
 import { IUser } from "dbmodels/User.interface";
 import { services } from "../../../api/services";
 
@@ -27,22 +26,3 @@ export const getUser = async (req: Request, res: Response) => {
     });
   }
 };
-
-// export async function getUser(req, res) {
-//   const { username } = req.params;
-//   try {
-//     if (!username) return res.status(501).send({ error: "Invalid Username" });
-//     UserModel.findOne({ username }, function (err, user) {
-//       if (err) return res.status(500).send({ err });
-//       if (!user)
-//         return res.status(501).send({ error: "Couldn't Find the User" });
-//       /** remove password from user */
-//       // mongoose return unnecessary data with object so convert it into json
-//       const { password, ...rest } = Object.assign({}, user.toJSON());
-
-//       return res.status(201).send(rest);
-//     });
-//   } catch (error) {
-//     return res.status(404).send({ error: "Cannot Find User Data" });
-//   }
-// }
---
+++ src/api/controllers/users/loginUser.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { services } from "../../../api/services";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET_KEY } from "../../../config/config";
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { username, password }: { username: string; password: string } = req.body;
+
+    const userInstance = await services.users.getUserInstanceByUsername(username, res);
+
+    // check wheather the password is empty or not
+    if (!password || password.length === 0) {
+      return res.json(StatusCodes.INTERNAL_SERVER_ERROR).send({
+        message: "Password is invalid or password is empty",
+      });
+    }
+
+    // Creating JWT Token
+    const jwtUserPayload = {
+      userId: userInstance._id,
+      username: userInstance.username,
+    };
+    const token = jwt.sign(jwtUserPayload, JWT_SECRET_KEY, { expiresIn: "24h" });
+
+    return res.send(StatusCodes.OK).send({
+      message: "Successfully Logged in",
+      username: userInstance.username,
+      token,
+    });
+  } catch (error) {
+    return res.json(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      error: "Cannot Find user Data",
+    });
+  }
+};
