03a4d35d7a31a424f883fb4a57729e3c394bf173
arkajitroy <arkajitroy11.official@gmail.com>
2024-03-22 19:50:51 +0530

    FEAT: added database connection + configurations

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,8 @@
+// CONSTANTS
+export const LOCAL_IP = "127.0.0.1";
 export const LOCAL_SERVER_PORT = 4000;
+export const LOCAL_MONGO_PORT = 27017;
+
+// DATABASE CONNECTIONS
+export const MONGODB_LOCAL_URI = `mongodb://${LOCAL_IP}:${LOCAL_MONGO_PORT}`;
+export const DATABASE_NAME = "node_server_backend";
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import compression from "compression";
 import bodyParser from "body-parser";
 import { LOCAL_SERVER_PORT } from "./config/config";
 import dotenv from "dotenv";
+import dbConnect from "./config/db.config";
 
 // Constants
 const app = express();
@@ -14,7 +15,13 @@ app.use(cors({ credentials: true }));
 app.use(compression());
 app.use(bodyParser.json());
 
-// Server Running
-app.listen(LOCAL_SERVER_PORT, () => {
-  console.log("The Backend Server is running @PORT", LOCAL_SERVER_PORT);
-});
+// Server and Database Connection
+dbConnect()
+  .then(() => {
+    app.listen(LOCAL_SERVER_PORT, () => {
+      console.log("The Backend Server is running @PORT", LOCAL_SERVER_PORT);
+    });
+  })
+  .catch((error: Error) => {
+    console.error("Failed to connect to the database:", error.message);
+  });
