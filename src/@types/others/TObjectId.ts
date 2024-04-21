a885ccf81e7da4e50e7662bcebb06f89c723a36b
arkajitroy <arkajitroy11.official@gmail.com>
2024-04-21 10:00:15 +0530

    Feat: add userModel and type declarations

---
+++ src/@types/others/TObjectId.ts
@@ -0,0 +1,5 @@
+import { Types } from 'mongoose'
+
+export type TObjectId = {
+    _id: Types.ObjectId
+}
---
+++ src/models/users.model.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema, Model } from 'mongoose';
+import { IUserSchema } from '@/@types/models/TUsers.schema';
+
+const UserSchema: Schema = new Schema({
+  username: {
+    type: String,
+    required: [true, 'Please provide a unique username'],
+    unique: [true, 'Username already exists'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    unique: false,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide a unique email'],
+    unique: true,
+  },
+  firstName: { type: String },
+  lastName: { type: String },
+  mobile: { type: Number },
+  address: { type: String },
+  profile: { type: String },
+});
+
+const UserModel: Model<IUserSchema> = mongoose.model<IUserSchema>('User', UserSchema);
+
+export default UserModel;
